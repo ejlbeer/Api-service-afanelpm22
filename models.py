b8a4f11c5ad77fc98865e869091eba1f3831ffdb
ejlbeer <80436640+ejlbeer@users.noreply.github.com>
2025-09-29 20:15:19 +0900

    Add files via upload

---
+++ models.py
@@ -0,0 +1,14 @@
+from . import db
+from datetime import datetime
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    body = db.Column(db.Text, default="")
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+
+    def __repr__(self):
+        return f"<Note {self.id} {self.title!r}>"
