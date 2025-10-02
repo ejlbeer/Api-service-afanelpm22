8817c93275776cb75e171ebc7dc973a4900c7d11
ejlbeer <80436640+ejlbeer@users.noreply.github.com>
2025-10-02 22:50:53 +0900

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,19 +6,20 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "dev-secret"  # для расширений Flask
+    app.config["SECRET_KEY"] = "dev-secret" 
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notes.sqlite3"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    CORS(app)   # разрешаем кросс-доменные запросы
+    CORS(app)  
     db.init_app(app)
-
-    # регистрация роутов
+    
     from .routes import bp
     app.register_blueprint(bp, url_prefix="/api")
-
-    # создание таблиц
+    @app.route("/")
+    def index():
+        return {"message": "добавьте /api/notes"}
     with app.app_context():
         db.create_all()
+
 
     return app
